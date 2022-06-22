ff24817d06a7246e397201680bdd405407c8e166
GonoBae <qldrhqorhsh@naver.com>
2022-06-22 19:17:43 +0900

    Photon

---
+++ MultiGame/Assets/Scripts/ServerManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ServerManager : MonoBehaviourPunCallbacks
+{
+	public override void OnJoinedLobby()
+	{
+		base.OnJoinedLobby();
+	}
+}
