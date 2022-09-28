8b2992dff6c6b073433dca7bfb78c002d3d53b30
GonoBae <qldrhqorhsh@naver.com>
2022-09-29 00:43:51 +0900

    맵 꾸미기

---
+++ MultiGame/Assets/Scripts/AI/AIManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class AIManager : MonoBehaviour
+{
+	private PhotonView _pv;
+	
+	private void Awake()
+	{
+		_pv = GetComponent<PhotonView>();
+	}
+	
+	private void Start()
+	{
+		if(_pv.IsMine)
+		{
+			CreateController();
+		}
+	}
+	
+	private void CreateController()
+	{
+		PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "AI"), new Vector3(Random.Range(-20, 30), 0, Random.Range(0, 100)) + Vector3.up, Quaternion.identity, 0, new object[] {_pv.ViewID});
+	}
+}
